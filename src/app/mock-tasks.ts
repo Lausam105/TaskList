ffd3388ca30d12e617e573c2d62f520c5ed30b71
Lausam105 <karimganimi22@gmail.com>
2022-05-10 21:50:31 -0300

    Agregado de tareas y su interface

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,27 @@
+import { Task } from "./Task"
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: "Mayo 15 a las 9:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Estudiar Angular",
+        day: "Mayo 15 a las 11:00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Profundizar sobre programación",
+        day: "Mayo 15 a las 13:00",
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Hacer la cena",
+        day: "Mayo 15 a las 21:00",
+        reminder: false
+    },
+]
